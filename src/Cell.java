89ef2a86626419a825ee0dd76eed17f2a5ab08c6
Yasmin Rodriguez <yasmin.rodriguez@gmail.com>
2017-11-09 12:05:44 -0500

    Changes to cell class and Flag enum

--- src/Cell.java
+++ src/Cell.java
@@ -8,28 +8,21 @@ public class Cell {
 	private String display;  // What the cell is displaying to the console
 	private Flag flag; //Enum indicating if the cell has a bomb or question mark
 	private int numberOfSurroundingMines; //number of mines around this cell from 0-8
-	private int row;
-	private int column;
-	private int cellNumber;
+	private int row; // number that represents the row the cell is in, it's 0 based
+	private int col; // number that represens the col the cell is in, it's 0 based
+	private int cellNumber; // cell number on the board, so if the board is 5 x 5, the last cell is 25
 	
-	
-	
-
 	// constructor
 	public Cell() {
-
 		covered = true;
 		display = "[ ]";
+		flag = null;
+		numberOfSurroundingMines = 0;
+		row = 0;
+		col = 0;
+		cellNumber = 0;
 	}
-	
-	public void setNumberOfSurroundingMines(int numOfMines) {
-		numberOfSurroundingMines = numOfMines;
-	}
-	
-	public int getNumberOfSurroundingMines() {
-		return numberOfSurroundingMines;
-	}
-	
+
 	// Use this method to change whether the cell is covered or not
 	public void changeCover() {
 		covered = ! covered;
@@ -50,7 +43,59 @@ public class Cell {
 		display = newDisplay;
 	}
 
-	// to string
+	// Sets the flag to bomb or question mark
+	public void setFlag(Flag newFlag) {
+		flag = newFlag;
+	}
 
+	// returns the current flag on the cell, null if it hasn't been flagged
+	public Flag getFlag() {
+		return flag;
+	}
 
+	// sets the number of mines surrounding this cell
+	public void setNumberOfSurroundingMines(int numOfMines) {
+		numberOfSurroundingMines = numOfMines;
+	}
+
+	// gets the number of mines surrounding this cell
+	public int getNumberOfSurroundingMines() {
+		return numberOfSurroundingMines;
+	}
+
+	// sets the row where this cell is
+	public void setRow(int rowNumber) {
+		row = rowNumber;
+	}
+
+	// gets the row where this cell is
+	public int getRow() {
+		return row;
+	}
+
+	// sets the column where this cell is
+	public void setColumn(int colNumber) {
+		col = colNumber;
+	}
+
+	// gets the column where this cell is
+	public int getColumn() {
+		return col;
+	}
+
+	// sets the cell number (position on the board)
+	public void setCellNumber(int cellNum) {
+		cellNumber = cellNum;
+	}
+
+	// gets the cell number
+	public int getCellNumber() {
+		return cellNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Cell [covered=" + covered + ", display=" + display + ", flag=" + flag + ", numberOfSurroundingMines="
+				+ numberOfSurroundingMines + ", row=" + row + ", col=" + col + ", cellNumber=" + cellNumber + "]";
+	}
 }
--- src/Flag.java
+++ src/Flag.java
@@ -1,4 +1,4 @@
 
 public enum Flag {
-
+	QUESTION("?"), BOMB("*");
 }
