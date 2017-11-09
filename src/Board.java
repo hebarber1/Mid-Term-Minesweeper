4eb2497be3930063dd24fe0548162327cfefeaa4
Timothy Johnson <pavementii@gmail.com>
2017-11-09 16:36:30 -0500

    added main class in Board to test printing

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 //Tim
 public class Board {
@@ -18,10 +19,12 @@ public class Board {
 	String[] alphabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O" };
 
 	Board(int boardSize) {
-
+		System.out.println("Running Board constructor...");
 		switch (boardSize) {
 		case 1:
+			System.out.println("Case 1...");
 			this.board = new Cell[SMALL_BOARD][SMALL_BOARD];
+			printBoard(this.board);
 			this.boardSize = SMALL_BOARD * SMALL_BOARD;
 			break;
 
@@ -43,6 +46,7 @@ public class Board {
 
 		placeMines(generateMines(this.boardSize));
 		initializeCells();
+		printBoard(this.board);
 
 	}
 
@@ -72,6 +76,7 @@ public class Board {
 	}
 
 	private int placeMines(ArrayList<Integer> listOfMines) {
+		System.out.println("Running placeMines()..");
 		for (Integer mine : listOfMines) {
 
 			for (int row = 0; row < this.board.length; row++) {
@@ -95,53 +100,54 @@ public class Board {
 
 	private void initializeCells() {
 
-		//set row, column, and cell number
-		for(int row = 0; row < this.board.length; row++) {
-			for(int column = 0; column < this.board[row].length; column++) {
-				
-				this.board[row][column].row = row;
-				this.board[row][column].column = column;
-				this.board[row][column].cellNumber = (row + 1) * (column + 1)
-						
-				if(row == 0) {
+		// set row, column, and cell number
+		for (int row = 0; row < this.board.length; row++) {
+			for (int column = 0; column < this.board[row].length; column++) {
+
+				this.board[row][column].setRow(row);
+				this.board[row][column].setColumn(column);
+				this.board[row][column].setCellNumber((row + 1) * (column + 1));
+
+				if (row == 0) {
 					this.board[row][column].isTopRow = true;
 				}
-				
-				if(row == this.board.length - 1) {
+
+				if (row == this.board.length - 1) {
 					this.board[row][column].isBottom = true;
 				}
-					
-				if(column == 0) {
+
+				if (column == 0) {
 					this.board[row][column].isLeftColumn = true;
 				}
-				
-				if(column == this.board[this.board.length-1].length) {
+
+				if (column == this.board[this.board.length - 1].length) {
 					this.board[row][column].isRightColumn = true;
 				}
-			}			
+			}
 		}
-		
+
 	}
 
 	/**
 	 * prints board with columns numbers and row letters so that cells can be
 	 * referenced alpha-numerically ("B12", etc)
 	 */
-	public void printBoard() {
-		String format1 = "%5s,";
+	public void printBoard(Cell[][] board ) {
+		System.out.println("Running printBoard()...");
+		String format1 = "%5s";
 
 		// print column numbers
-		for (int column = 0; column < this.board[0].length; column++) {
+		for (int column = 0; column < board[0].length; column++) {
 			System.out.print(String.format(format1, (column + 1)));
 		}
 
 		// print rows with leading letter
-		for (int row = 0; row < this.board.length; row++) {
+		for (int row = 0; row < board.length; row++) {
 			// print the row Letter
 			System.out.print(String.format("%5s", alphabet[row]));
 			// print cells
-			for (int column = 0; column < this.board[row].length; column++) {
-				System.out.print(String.format("%5s", this.board[row][column].getDisplay()));
+			for (int column = 0; column < board[row].length; column++) {
+				System.out.print(String.format("%5s", board[row][column].getDisplay()));
 
 			}
 		}
@@ -161,6 +167,20 @@ public class Board {
 		}
 
 		return numberOfMines;
+
+	}
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		boolean keepGoing = true;
+
+		System.out.println("Welcome to Ti-Yas-Man Minesweeper!\n");
+
+		Board mineBoard = new Board(1); // TOFIX Specify board size
+
+		mineBoard.generateBoard();
+		mineBoard.printBoard(mineBoard.board);
 
 	}
 	// CONSTRUCTOR
